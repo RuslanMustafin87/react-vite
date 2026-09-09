555d888c333f0c599ef147c727b2c3aeb99c7969
Ruslan <ruslanmust87@gmail.com>
2026-09-10 00:47:26 +0500

    fetch and spinner

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
-// import { useState } from 'react'
-// import heroImg from './assets/hero.png'
+import { useEffect, useState } from 'react'
+import Spinner from "./Spinner.jsx"
 
 import "./App.scss";
 
 function App() {
+  const [people, setPeople] = useState({});
+
+  useEffect(() => {
+    fetch("https://reqres.in/api/users?delay=1.5")
+      .then( response => response.json())
+      .then( data => setPeople(data))
+      .catch(() => console.log("Нет данных"))
+  },[])
 
   return (
     <>
-      <p className="hello">hello</p>
+      <div className="container">
+        <div className="box-center">       
+            {
+              people.data ? 
+                <ul> {people.data.map((item) => (
+                  <li key={item.id}>{item.first_name}</li>))} 
+                </ul> 
+              : <Spinner /> 
+            }
+        </div>
+      </div>
     </>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // server: {
+  //   proxy: {
+  //     // Все запросы, начинающиеся с /api, будут перенаправляться
+  //     '/api': {
+  //       target: 'https://app.reqres.in',
+  //       changeOrigin: true,
+  //       // Убираем префикс /api перед отправкой на целевой сервер
+  //       rewrite: (path) => path.replace(/^\/api/, ''), 
+  //     },
+  //   },
+  // },
 })
